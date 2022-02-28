d88180e1786b8117b004861f76d88c957bb067f7
sergio <sergiosantanadosreis@hotmail.com>
2022-02-28 12:40:20 -0300

    Introdução ao unittest. Usando e entendendo os conhecimentos de herança. Escrevendo o teste do modulo carro.py. Renomeando adequadamente os módulos do pacote testes, os quais são obrigados a usar o prefixo 'teste'/'test' para rodar.

---
+++ oo/teste_carro.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+
+from oo.carro import Motor
+
+class CarroTestCase(TestCase):
+    def teste_velocidade_inicial(self): #é preciso usar o prefixo 'teste' ou 'test'
+        motor = Motor()
+        self.assertEqual(0, motor.velocidade)
+    def teste_acelerar(self):
+        motor = Motor()
+        motor.acelerar()
+        self.assertEqual(1, motor.velocidade)
+    def teste_frear(self):
+        motor = Motor()
+        motor.frear()
+        self.assertEqual(0, motor.velocidade)
+
+
+
+
+
